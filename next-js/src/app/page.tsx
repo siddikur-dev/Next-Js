f5f1b555262dfe12dcb5029aa688b07d2632bd73
siddikur-dev <siddikur.dev@gmail.com>
2025-08-22 16:54:15 +0600

    alert Showing

--- next-js/src/app/page.tsx
+++ next-js/src/app/page.tsx
@@ -1,11 +1,11 @@
-
-
+"use client";
 export default function Home() {
+  const alertFun = () => {
+    alert("Hey this is Next Js");
+  };
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h2>Hey this is Next Js </h2>
-      </main>
-    </div>
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <button onClick={alertFun}>Show Alert</button>
+    </main>
   );
 }
