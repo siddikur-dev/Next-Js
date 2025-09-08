92c6cf388b512493cdbf893aaf086c61c98519d2
siddikur-dev <siddikur.dev@gmail.com>
2025-09-08 15:49:15 +0600

    change name clicked by useState

--- next-js/src/app/state.jsx
+++ next-js/src/app/state.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useState } from "react";
 const state = () => {
   const [value, setValue] = useState(0);
+  const [name, setName] = useState("SIAM");
   const add = () => {
     setValue(value + 1);
   };
@@ -12,12 +13,19 @@ const state = () => {
   const reset = () => {
     setValue("");
   };
+  const changeName = () => {
+    setName("Siddikur Rahman");
+  };
   return (
     <div>
       <h2 className="btn-primary">Count Here : {value}</h2>
       <button onClick={add}>Add</button>
       <button onClick={minus}>Minus</button>
       <button onClick={reset}>Reset</button>
+
+      <h2 className="btn-primary"> {name}</h2>
+
+      <button onClick={changeName}>Change Name</button>
     </div>
   );
 };
