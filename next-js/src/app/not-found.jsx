a3d491b833d9de35bb3d2653286c2e7ea5e89833
siddikur-dev <siddikur.dev@gmail.com>
2025-09-09 10:52:54 +0600

    custom 404 page created

---
+++ next-js/src/app/not-found.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div>
+      <section class=" notFound flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
+        <div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+          <div class="max-w-md text-center">
+            <h1 class="mb-8 font-extrabold text-9xl dark:text-gray-400">
+              <span class="">Error</span>404
+            </h1>
+            <p class="text-2xl font-semibold md:text-3xl">
+              Sorry, we couldn't find this page.
+            </p>
+            <p class="mt-4 mb-8 dark:text-gray-600">
+              But dont worry, you can find plenty of other things on our
+              homepage.
+            </p>
+            <a
+              rel="noopener noreferrer"
+              href="./layout.tsx"
+              class="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+            >
+              Back to homepage
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default notFound;
